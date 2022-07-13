f932a16eb58d0811127101f041155f4622dadd72
이은지 <406greendg5532@gmail.com>
2022-07-13 15:01:28 +0900

    220713 수정

--- js/content.js
+++ js/content.js
@@ -23,4 +23,9 @@ $(document).ready(function(){
     $(".left ul li").eq(2).addClass("animated fadeInLeft").css("opacity",1).css("transition-delay","1000ms").css("animation-delay","1000ms");
 
 
+    $(".center ul li").eq(0).addClass("animated bounceInUp").css("opacity",1).css("transition-delay","1100ms").css("animation-delay","1100ms");
+    $(".center ul li").eq(1).addClass("animated bounceInUp").css("opacity",1).css("transition-delay","1500ms").css("animation-delay","1500ms");
+    $(".center ul li").eq(2).addClass("animated zoomIn").css("opacity",1).css("transition-delay","1800ms").css("animation-delay","1800ms");
+    $(".center ul li").eq(3).addClass("animated zoomIn").css("opacity",1).css("transition-delay","2000ms").css("animation-delay","2000ms");
+
 })//jquery
